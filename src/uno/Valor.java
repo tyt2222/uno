f9c990c6760edcc3c5569bafbfc8767c14faf7ea
tyt2222 <fabio.paiva.dev@gmail.com>
2026-06-11 18:59:57 -0300

    arquivos iniciais

---
+++ src/uno/Valor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uno;
+
+public enum Valor {
+    ZERO,
+    UM,
+    DOIS,
+    TRES,
+    QUATRO,
+    CINCO,
+    SEIS,
+    SETE,
+    OITO,
+    NOVE
+}
