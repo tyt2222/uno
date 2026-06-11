e9872480e433af708f3f13160adba38894ce781d
tyt2222 <fabio.paiva.dev@gmail.com>
2026-06-11 19:12:21 -0300

    Classes iniciais feitas, falta o controlador principalk

--- src/uno/Carta.java
+++ src/uno/Carta.java
@@ -5,5 +5,34 @@
 package uno;
 
 public class Carta {
-    public
+
+    private Cor cor;
+    private Valor valor;
+
+    public Carta(Cor cor, Valor valor) {
+        setCor(cor);
+        setValor(valor);
+    }
+
+    @Override
+    public String toString() {
+        return getValor() + "-" + getCor(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    private void setCor(Cor cor) {
+        this.cor = cor;
+    }
+
+    private void setValor(Valor valor) {
+        this.valor = valor;
+    }
+
+    private Cor getCor() {
+        return cor;
+    }
+
+    private Valor getValor() {
+        return valor;
+    }
+
 }
---
+++ src/uno/Jogador.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+    private String nome;
+    private List<Carta> mao;
+
+    public Jogador(String nome, List<Carta> mao) {
+        setNome(nome);
+        this.mao = new ArrayList<>();
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Carta> getMao() {
+        return mao;
+    }
+    
+    
+    
+    
+}
